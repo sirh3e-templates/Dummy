c5d307dda50cc587ec8d8383dc2c439fb334adfe
sirh3e <sirh3e@protonmail.com>
2021-03-26 19:54:18 +0100

    [ADDED] src/Sirh3e.Dummy/Foo.cs

---
+++ src/Sirh3e.Dummy/Foo.cs
@@ -0,0 +1,19 @@
+﻿namespace Sirh3e.Dummy
+{
+    public class Foo<T>
+    {
+        public T? Value { get; set; }
+        
+        public static class Factory
+        {
+            public static Foo<T> CreateDefault()
+                => new();
+            
+            public static Foo<T> CreateDefaultByValue(T value)
+                => new()
+                {
+                    Value = value
+                };
+        }
+    }
+}
